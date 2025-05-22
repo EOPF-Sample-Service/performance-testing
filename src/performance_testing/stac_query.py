82c346a0e0f3dd5789d91841a962f0f0b398d4a2
Christoph Reimer <christoph.reimer@eodc.eu>
2025-05-22 21:10:01 +0200

    configure locust to exit with code 0.

--- src/performance_testing/stac_query.py
+++ src/performance_testing/stac_query.py
@@ -11,14 +11,7 @@ from locust import events
 
 @events.quitting.add_listener
 def _(environment, **kw):
-    if environment.stats.total.fail_ratio > 0.01:
-        logging.error("Test failed due to failure ratio > 1%")
-        environment.process_exit_code = 1
-    elif environment.stats.total.get_response_time_percentile(0.95) > 2000:
-        logging.error("Test failed due to 95th percentile response time > 2s")
-        environment.process_exit_code = 1
-    else:
-        environment.process_exit_code = 0
+    environment.process_exit_code = 0
 
 
 class STACUser(HttpUser):
